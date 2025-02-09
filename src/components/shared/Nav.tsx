10431ea00009944f8e9772fa317d1fba46ce38bd
Dodul01 <mhdodul299@gmail.com>
2025-02-09 12:37:46 +0600

    Project setup completed

---
+++ src/components/shared/Nav.tsx
@@ -0,0 +1,15 @@
+
+/**
+ * home page
+ * project page
+ * blog page
+ * contact page
+ * login page [not sure]
+ * register page [not sure]
+ * 
+ * */ 
+const Nav = () => {
+  return <nav></nav>;
+};
+
+export default Nav;
